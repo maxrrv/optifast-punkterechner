7e01e15ee0a012ecc4d5519e9ef120507c0f1478
maximilian.ungar <maximilian.ungar@klarna.com>
2020-04-08 22:54:57 +0200

    Run Prettier. Add Calculator.vue to components

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <Calculator />
+    <Calculator msg="Hello World" />
   </div>
 </template>
 
 <script>
-import Calculator from "./components/Calculator.vue";
+import Calculator from './components/Calculator.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    Calculator
-  }
-};
+    Calculator,
+  },
+}
 </script>
 
 <style lang="scss">
---
+++ project/src/components/Calculator.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-on:input="changeTitle">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  methods: {
+    changeTitle: function(event) {
+      this.msg = event.target.value
+    },
+  },
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<style scoped>
+.demo {
+  background-color: cyan;
+}
+</style>
